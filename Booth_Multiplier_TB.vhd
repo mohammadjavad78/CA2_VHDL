679ec4ebfb9f9b8f2daf9de616607103677e3c77
mohammadjavad78 <mohammadjavadzare1378@yahoo.com>
2022-04-27 15:29:29 +0430

    changed

---
+++ Booth_Multiplier_TB.vhd
@@ -0,0 +1,46 @@
+	LIBRARY ieee;     
+	use IEEE.std_logic_1164.all;
+	use IEEE.STD_LOGIC_UNSIGNED.all;
+
+	ENTITY BoothMul_TB is
+	END BoothMul_TB;
+	ARCHITECTURE TB of BoothMul_TB is
+		SIGNAL clk :  STD_LOGIC := '0';
+		SIGNAL rst:  STD_LOGIC := '1';
+		SIGNAL X :  STD_LOGIC_VECTOR (7 DOWNTO 0);
+		SIGNAL Y :  STD_LOGIC_VECTOR (7 DOWNTO 0);
+		SIGNAL startMUL :  STD_LOGIC :='0';
+		SIGNAL OUTR4 :  STD_LOGIC_VECTOR(15 DOWNTO 0);
+		SIGNAL completeR4 :  STD_LOGIC;
+	BEGIN
+			--Unit Under Test Instantiation:
+		UUT : ENTITY work.booth_multiplier
+		PORT MAP (clk => clk, rst => rst, in1 => X,
+		in2 => Y , start=> startMUL,
+		out1 => OUTR4,completeR4=>completeR4);
+		-- clk generation:
+		clk <=  not clk after 10 ns;
+		tb1 : process
+		begin
+			rst <= '0' after 80 ns;
+			wait for 90 ns;startMUL <= '1'; X <= x"50";Y <= x"62";
+			wait for 20 ns;startMUL <= '0'; 
+			-- wait for 20 ns;xInput <= x"0003";wait for 20 ns;xInput <= x"0004";
+			-- --changing the degree value in the middle of inpt application
+			-- --this MUST NOT affect the calculation process
+			-- Degree <= "0011";
+			-- wait for 20 ns;xInput <= x"0005";wait for 20 ns;xInput <= x"0006";			
+			-- wait for 20 ns;xInput <= x"0007";
+			-- -- applying new set of data with a different degree
+			-- Degree <= "0100"; 
+			-- wait for 90 ns;Start <= '1'; wait for 20 ns; Start <= '0';
+			-- wait for 20 ns;xInput <= x"0004";wait for 20 ns;xInput <= x"0003";
+			-- wait for 20 ns;xInput <= x"0002";wait for 20 ns;xInput <= x"0001";
+			
+		   wait;
+		end process;
+	END TB ;
+	
+	
+	
+
--- booth_multiplier.vhd
+++ booth_multiplier.vhd
@@ -15,37 +15,36 @@ ENTITY booth_multiplier_datapath IS
         FINALREG_rst2:IN STD_LOGIC;
         FINALREG_loaden1:IN STD_LOGIC;
         FINALREG_loaden2:IN STD_LOGIC;
-        out1:OUT STD_LOGIC_VECTOR(bits-1 DOWNTO 0)
+        out1:OUT STD_LOGIC_VECTOR(2*bits-1 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE radix4 OF booth_multiplier_datapath IS 
-    SIGNAL AXOR:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
-    SIGNAL AXOR0:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
     SIGNAL AREGOUT:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
     SIGNAL AREGOUT0:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
     SIGNAL MUX3_out:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
-    SIGNAL FINALREG_out1:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
+    SIGNAL MUX3_out_2:STD_LOGIC_VECTOR(bits+1 DOWNTO 0);
+    SIGNAL FINALREG_out1:STD_LOGIC_VECTOR(bits+1 DOWNTO 0);
     SIGNAL FINALREG_out1_2:STD_LOGIC_VECTOR(1 DOWNTO 0);
-    SIGNAL FINALREG_out2:STD_LOGIC_VECTOR(bits DOWNTO 0);
-    SIGNAL add_sub_out:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
-    SIGNAL add_sub_out_shifted:STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
-    SIGNAL in2_0:STD_LOGIC_VECTOR(bits DOWNTO 0);
+    SIGNAL FINALREG_out2:STD_LOGIC_VECTOR(2*bits DOWNTO 0);
+    SIGNAL add_sub_out:STD_LOGIC_VECTOR(bits+1 DOWNTO 0);
+    SIGNAL add_sub_out_shifted:STD_LOGIC_VECTOR(bits+1 DOWNTO 0);
+    SIGNAL in2_0:STD_LOGIC_VECTOR(2*bits DOWNTO 0);
 begin
-    FINALREG_out1_2<=(FINALREG_out1(bits-1),FINALREG_out1(bits-1));
-    in2_0<=in2&'0';
+    FINALREG_out1_2<=(FINALREG_out1(bits+1),FINALREG_out1(bits+1));
+    ui:for i in 2*bits downto bits+1 generate begin 
+    in2_0(i)<=in2(bits-1);
+    end generate;
+    in2_0(bits downto 0)<=in2&'0';
+    MUX3_out_2<=AREGOUT(bits-1)&AREGOUT(bits-1)&MUX3_out;
     AREGOUT0<=AREGOUT(bits-2 DOWNTO 0)&'0';
-    out1<=FINALREG_out2(bits DOWNTO 1);
-    add_sub_out_shifted<=add_sub_out(bits-1)&add_sub_out(bits-1)&add_sub_out(bits-1 DOWNTO 2);
+    out1<=FINALREG_out1(bits-1 DOWNTO 0)&FINALREG_out2(2*bits DOWNTO bits+1);
+    add_sub_out_shifted<=(add_sub_out(bits)&add_sub_out(bits)&add_sub_out(bits+1 DOWNTO 2));
     AREG:ENTITY WORK.reg GENERIC MAP(bits=>bits) PORT MAP(in1=>in1,en=>AREG_en,clk=>clk,rst=>AREG_rst,loaden=>AREG_loaden,out1=>AREGOUT);
-    ff:FOR i IN 0 TO bits-1 GENERATE 
-        AXOR(i)<=AREGOUT(i) XOR '1';
-        AXOR0(i)<=AREGOUT0(i) XOR '1';
-    END GENERATE;
     MUX3:ENTITY WORK.mux3 GENERIC MAP(bits=>bits) PORT MAP(in1=>(OTHERS=>'0'),in2=>AREGOUT,in3=>AREGOUT,in4=>AREGOUT0,in5=>AREGOUT0,in6=>AREGOUT,in7=>AREGOUT,in8=>(OTHERS=>'0'),cin=>FINALREG_out2(2 DOWNTO 0),out1=>MUX3_out);
-    ADDSUB:ENTITY WORK.add_sub GENERIC MAP(bits=>bits) PORT MAP(in1=>FINALREG_out1,in2=>MUX3_out,cin=>FINALREG_out2(2),out1=>add_sub_out);
-    FINALREG1:ENTITY WORK.shift_reg GENERIC MAP(bits=>bits) PORT MAP(in1=>add_sub_out_shifted,inbit=>FINALREG_out1_2,en=>FINALREG_en1,clk=>clk,rst=>FINALREG_rst1,loaden=>FINALREG_loaden1,out1=>FINALREG_out1);
-    FINALREG2:ENTITY WORK.shift_reg GENERIC MAP(bits=>bits+1) PORT MAP(in1=>in2_0,inbit=>add_sub_out(1 DOWNTO 0),en=>FINALREG_en2,clk=>clk,rst=>FINALREG_rst2,loaden=>FINALREG_loaden2,out1=>FINALREG_out2);
+    ADDSUB:ENTITY WORK.add_sub GENERIC MAP(bits=>bits+2) PORT MAP(in1=>FINALREG_out1,in2=>MUX3_out_2,cin=>FINALREG_out2(2),out1=>add_sub_out);
+    FINALREG1:ENTITY WORK.shift_reg GENERIC MAP(bits=>bits+2) PORT MAP(in1=>add_sub_out_shifted,inbit=>FINALREG_out1_2,en=>FINALREG_en1,clk=>clk,rst=>FINALREG_rst1,loaden=>FINALREG_loaden1,out1=>FINALREG_out1);
+    FINALREG2:ENTITY WORK.shift_reg GENERIC MAP(bits=>2*bits+1) PORT MAP(in1=>in2_0,inbit=>add_sub_out(1 DOWNTO 0),en=>FINALREG_en2,clk=>clk,rst=>FINALREG_rst2,loaden=>FINALREG_loaden2,out1=>FINALREG_out2);
 END ARCHITECTURE;
 
 
@@ -69,7 +68,8 @@ ENTITY booth_multiplier_controller IS
         FINALREG_loaden1:OUT STD_LOGIC;
         FINALREG_loaden2:OUT STD_LOGIC;
         FINALREG_en2:OUT STD_LOGIC;
-        FINALREG_rst2:OUT STD_LOGIC
+        FINALREG_rst2:OUT STD_LOGIC;
+        completeR4:OUT STD_LOGIC
     );
 END ENTITY;
 
@@ -101,6 +101,7 @@ PROCESS(currentstate,start,ZEROS(integer(ceil(log2(real(bits))))+1)) BEGIN
     FINALREG_rst1<='0';
     FINALREG_en2<='0';
     FINALREG_rst2<='0';
+    completeR4<='0';
     load_en<='0';
     CASE currentstate IS 
         WHEN s0=>
@@ -111,7 +112,7 @@ PROCESS(currentstate,start,ZEROS(integer(ceil(log2(real(bits))))+1)) BEGIN
             FINALREG_rst1<='1';
             counter_en<='1';
             load_en<='1';
-            load<=STD_LOGIC_VECTOR(TO_UNSIGNED(bits/2-1,load'LENGTH));
+            load<=STD_LOGIC_VECTOR(TO_UNSIGNED(bits/2,load'LENGTH));
             IF(start='1') THEN
                 nextstate<=s1;
             END IF;
@@ -121,7 +122,7 @@ PROCESS(currentstate,start,ZEROS(integer(ceil(log2(real(bits))))+1)) BEGIN
             FINALREG_loaden2<='1';
             counter_en<='1';
             load_en<='1';
-            load<=STD_LOGIC_VECTOR(TO_UNSIGNED(bits/2-1,load'LENGTH));
+            load<=STD_LOGIC_VECTOR(TO_UNSIGNED(bits/2,load'LENGTH));
             counter_en<='1';
         WHEN s2=>
             counter_en<='1';
@@ -131,11 +132,10 @@ PROCESS(currentstate,start,ZEROS(integer(ceil(log2(real(bits))))+1)) BEGIN
             FINALREG_loaden2<='0';
             IF(ZEROS(integer(ceil(log2(real(bits))))+1)='0') THEN
                 nextstate<=s0;
+                completeR4<='1';
             ELSE
                 nextstate<=s2;
             END IF;
-        -- WHEN OTHERS=>
-        --     nextstate<=s0;
     END CASE;
     END PROCESS;
     PROCESS(clk,rst) BEGIN 
@@ -154,13 +154,14 @@ LIBRARY IEEE;
     USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY booth_multiplier IS 
-    GENERIC(bits:INTEGER:=6);
+    GENERIC(bits:INTEGER:=8);
     PORT(
         in1,in2:IN STD_LOGIC_VECTOR(bits-1 DOWNTO 0);
         clk:IN STD_LOGIC;
         start:IN STD_LOGIC;
         rst:IN STD_LOGIC;
-        out1:OUT STD_LOGIC_VECTOR(bits-1 DOWNTO 0)
+        out1:OUT STD_LOGIC_VECTOR(2*bits-1 DOWNTO 0);
+        completeR4:OUT STD_LOGIC
     );
 END ENTITY;
 
@@ -176,5 +177,5 @@ ARCHITECTURE radix4 OF booth_multiplier IS
     SIGNAL FINALREG_rst2:STD_LOGIC;
 begin
     datapath:ENTITY WORK.booth_multiplier_datapath GENERIC MAP(bits=>bits) PORT MAP(in1=>in1,in2=>in2,clk=>clk,AREG_en=>AREG_en,AREG_rst=>AREG_rst,AREG_loaden=>AREG_loaden,FINALREG_en1=>FINALREG_en1,FINALREG_rst1=>FINALREG_rst1,FINALREG_en2=>FINALREG_en2,FINALREG_rst2=>FINALREG_rst2,FINALREG_loaden1=>FINALREG_loaden1,FINALREG_loaden2=>FINALREG_loaden2,out1=>out1);
-    controller:ENTITY WORK.booth_multiplier_controller GENERIC MAP(bits=>bits) PORT MAP(clk=>clk,start=>start,rst=>rst,AREG_en=>AREG_en,AREG_rst=>AREG_rst,AREG_loaden=>AREG_loaden,FINALREG_en1=>FINALREG_en1,FINALREG_rst1=>FINALREG_rst1,FINALREG_loaden1=>FINALREG_loaden1,FINALREG_loaden2=>FINALREG_loaden2,FINALREG_en2=>FINALREG_en2,FINALREG_rst2=>FINALREG_rst2);
+    controller:ENTITY WORK.booth_multiplier_controller GENERIC MAP(bits=>bits) PORT MAP(clk=>clk,start=>start,rst=>rst,AREG_en=>AREG_en,AREG_rst=>AREG_rst,AREG_loaden=>AREG_loaden,FINALREG_en1=>FINALREG_en1,FINALREG_rst1=>FINALREG_rst1,FINALREG_loaden1=>FINALREG_loaden1,FINALREG_loaden2=>FINALREG_loaden2,FINALREG_en2=>FINALREG_en2,FINALREG_rst2=>FINALREG_rst2,completeR4=>completeR4);
 END ARCHITECTURE;
